f701edfdb1352e065d5daf76079a8d3c4da4526b
Javinator9889 <dev@javinator9889.com>
2021-05-05 12:51:04 +0200

    utils: added documentation

--- Src/utils.c
+++ Src/utils.c
@@ -18,11 +18,31 @@
  */
 #include "utils.h"
 
-//función auxiliar de estandarización de valores:
+/**
+ * @brief Maps a given value in between a given proportional range.
+ * 
+ * @param x         the value to map.
+ * @param in_min    the minimum input value to map.
+ * @param in_max    the maximum input value to map.
+ * @param out_min   the minimum output value to produce.
+ * @param out_max   the maximum output value to produce.
+ * @return int - the 'x' value mapped in between [out_min, out_max].
+ */
 int map(int x, int in_min, int in_max, int out_min, int out_max) {
   return (int)((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
 }
 
+/**
+ * @brief With the given float value, produces the equivalent 4 bytes
+ *        representing that value.
+ * 
+ *        Notice that this function relies on that a float is 4 bytes
+ *        in memory. Higher (or lower) values will require this method
+ *        to be overwritten.
+ * 
+ * @param value the input float to convert.
+ * @param bytes the output bytes array (4) to produce.
+ */
 void f2b(float value, uint8_t* bytes[4]) {
     union {
         float float_var;
@@ -33,6 +53,17 @@ void f2b(float value, uint8_t* bytes[4]) {
     memcpy(bytes, u.tmp_arr, 4);
 }
 
+/**
+ * @brief With the given bytes array, produces the equivalent float value
+ *        represented by that 4 bytes.
+ * 
+ *        Notice that this function relies on that a float is 4 bytes
+ *        in memory. Higher (or lower) values will require this method
+ *        to be overwritten.
+ * 
+ * @param bytes the input bytes array (4) to read.
+ * @return float - the converted float data from bytes.
+ */
 float b2f(uint8_t* bytes[4]) {
     union {
         float val;

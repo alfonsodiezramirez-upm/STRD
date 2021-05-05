3985b2fb497a1bcfc65aa47d211e0c7f91ba7433
Javinator9889 <dev@javinator9889.com>
2021-05-05 10:12:58 +0200

    lock: clean-up code and configured option to create static mutexes

--- Src/lock.c
+++ Src/lock.c
@@ -21,172 +21,34 @@
 #include <FreeRTOSConfig.h>
 #include <portmacro.h>
 #include <projdefs.h>
+#include <semphr.h>
 #include "task.h"
 
-#define staticDONT_BLOCK ((TickType_t)0)
-#define staticBINARY_SEMAPHORE_MAX_COUNT (1)
 
-static volatile BaseType_t xErrorOccurred = pdFALSE;
-static volatile SemaphoreHandle_t _lock = NULL;
-static volatile StaticSemaphore_t _buff;
-
-/*static void LOCK_lock_acquire(void)
-{
-    if (_lock == NULL)
-    {
-        taskDISABLE_INTERRUPTS();
-        _lock = xSemaphoreCreateMutexStatic(&_buff);
-        taskENABLE_INTERRUPTS();
-    }
-    BaseType_t xResult = xSemaphoreTake(_lock, portMAX_DELAY);
-    configASSERT(xResult == pdPASS);
-}
-
-static void LOCK_lock_release(void)
-{
-    if (_lock != NULL)
-    {
-        xSemaphoreGive(_lock);
-    }
-}*/
-
-/**
- * @brief Based on: https://sourceforge.net/p/freertos/code/HEAD/tree/trunk/FreeRTOS/Demo/Common/Minimal/StaticAllocation.c#l893
- * 
- * @param xSemaphore semaphore to check
- * @param uxMaxCount maximum takes that can happen
- */
-static void prvSanityCheckCreatedSemaphore(SemaphoreHandle_t xSemaphore, UBaseType_t uxMaxCount)
-{
-    BaseType_t xReturned;
-    UBaseType_t x;
-    const TickType_t xShortBlockTime = pdMS_TO_TICKS(10);
-    TickType_t xTickCount;
-
-    /* The binary semaphore should start 'empty', so a call to xSemaphoreTake()
-	should fail. */
-    xTickCount = xTaskGetTickCount();
-    xReturned = xSemaphoreTake(xSemaphore, xShortBlockTime);
-
-    if (((TickType_t)(xTaskGetTickCount() - xTickCount)) < xShortBlockTime)
-    {
-        /* Did not block on the semaphore as long as expected. */
-        xErrorOccurred = pdTRUE;
-    }
-
-    if (xReturned != pdFAIL)
-    {
-        xErrorOccurred = pdTRUE;
-    }
-
-    /* Should be possible to 'give' the semaphore up to a maximum of uxMaxCount
-	times. */
-    for (x = 0; x < uxMaxCount; x++)
-    {
-        xReturned = xSemaphoreGive(xSemaphore);
-
-        if (xReturned == pdFAIL)
-        {
-            xErrorOccurred = pdTRUE;
-        }
-    }
-
-    /* Giving the semaphore again should fail, as it is 'full'. */
-    xReturned = xSemaphoreGive(xSemaphore);
-
-    if (xReturned != pdFAIL)
-    {
-        xErrorOccurred = pdTRUE;
-    }
-
-    configASSERT(uxSemaphoreGetCount(xSemaphore) == uxMaxCount);
-
-    /* Should now be possible to 'take' the semaphore up to a maximum of
-	uxMaxCount times without blocking. */
-    for (x = 0; x < uxMaxCount; x++)
-    {
-        xReturned = xSemaphoreTake(xSemaphore, staticDONT_BLOCK);
-
-        if (xReturned == pdFAIL)
-        {
-            xErrorOccurred = pdTRUE;
-        }
-    }
-
-    /* Back to the starting condition, where the semaphore should not be
-	available. */
-    xTickCount = xTaskGetTickCount();
-    xReturned = xSemaphoreTake(xSemaphore, xShortBlockTime);
-
-    if (((TickType_t)(xTaskGetTickCount() - xTickCount)) < xShortBlockTime)
-    {
-        /* Did not block on the semaphore as long as expected. */
-        xErrorOccurred = pdTRUE;
-    }
-
-    if (xReturned != pdFAIL)
-    {
-        xErrorOccurred = pdTRUE;
-    }
-
-    configASSERT(uxSemaphoreGetCount(xSemaphore) == 0);
-}
-
-SemaphoreHandle_t LOCK_create(StaticSemaphore_t *mutexBuffer)
-{
-    //LOCK_lock_acquire();
+SemaphoreHandle_t LOCK_create(StaticSemaphore_t *mutexBuffer) {
     SemaphoreHandle_t xSemaphore = NULL;
     BaseType_t xReturned;
+
+    #if defined(configSUPPORT_STATIC_ALLOCATION) && (configSUPPORT_STATIC_ALLOCATION == 1)
+    if (mutexBuffer != NULL) xSemaphore = xSemaphoreCreateMutexStatic(mutexBuffer);
+    else
+    #endif
+    xSemaphore = xSemaphoreCreateMutex();
+
+    configASSERT(xSemaphore != NULL);    
+    configASSERT(xSemaphoreGive(xSemaphore) != pdTRUE);
     
-        xSemaphore = xSemaphoreCreateMutex();
-        /* The semaphore handle should equal the static semaphore structure
-		passed into the xSemaphoreCreateMutexStatic() function. */
-        configASSERT(xSemaphore != NULL);    
-		configASSERT(xSemaphoreGive(xSemaphore) != pdTRUE);
-    
-   /* xReturned = xSemaphoreTake(xSemaphore, staticDONT_BLOCK);
-    if (xReturned != pdPASS)
-    {
-        xErrorOccurred = pdTRUE;
-    }
-    prvSanityCheckCreatedSemaphore(xSemaphore, staticBINARY_SEMAPHORE_MAX_COUNT);
-    configASSERT(xErrorOccurred == pdTRUE);*/
-    xErrorOccurred = pdFALSE;
-    //LOCK_lock_release();
     return xSemaphore;
 }
 
-void LOCK_destroy(SemaphoreHandle_t sem)
-{
+inline void LOCK_destroy(SemaphoreHandle_t sem) {
     vSemaphoreDelete(sem);
 }
 
-long LOCK_acquire(SemaphoreHandle_t sem)
-{
+inline long LOCK_acquire(SemaphoreHandle_t sem) {
     return xSemaphoreTake(sem, portMAX_DELAY);
 }
 
-void LOCK_release(SemaphoreHandle_t sem)
-{
+inline void LOCK_release(SemaphoreHandle_t sem) {
     xSemaphoreGive(sem);
 }
-
-// void *LOCK_read(pobject_t *container) {
-//    if (xSemaphoreTake(container->lock, portMAX_DELAY) == pdTRUE) {
-//         void *dest = container->data;
-//         xSemaphoreGive(container->lock);
-//         return dest;
-//     } else {
-//         return NULL;
-//     }
-// }
-
-// char LOCK_write(pobject_t *container, void *value) {
-//     if (xSemaphoreTake(container->lock, portMAX_DELAY) == pdTRUE) {
-//         container->data = value;
-//         xSemaphoreGive(container->lock);
-//         return 0;
-//     } else {
-//         return 1;
-//     }
-// }

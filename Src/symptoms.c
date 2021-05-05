75bf9b26a6c35bac824b28b551af2c5e88807e8a
Javinator9889 <dev@javinator9889.com>
2021-05-05 11:00:12 +0200

    symptoms: use new LOCK methods

--- Src/symptoms.c
+++ Src/symptoms.c
@@ -24,8 +24,8 @@
 #include <task.h>
 #include <stdbool.h>
 
-static SemaphoreHandle_t SYMPTOMS1_sem = NULL;
-static SemaphoreHandle_t SYMPTOMS2_sem = NULL;
+static Lock_t SYMPTOMS1_sem = NULL;
+static Lock_t SYMPTOMS2_sem = NULL;
 
 static float GIROSCOPE_x = .0F;
 static float GIROSCOPE_y = .0F;
@@ -40,78 +40,62 @@ void SYMPTOMS_init(void) {
 }
 
 void GIROSCOPE_set(float x, float y, float z) {
-    configASSERT(SYMPTOMS1_sem != NULL);
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        GIROSCOPE_x = x;
-        GIROSCOPE_y = y;
-        GIROSCOPE_z = z;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
-    
+    LOCK_acquire(SYMPTOMS1_sem);
+    GIROSCOPE_x = x;
+    GIROSCOPE_y = y;
+    GIROSCOPE_z = z;
+    LOCK_release(SYMPTOMS1_sem);
 }
+
 float GIROSCOPE_get_X(void) {
-    configASSERT(SYMPTOMS1_sem != NULL);
     float x = -1;
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        x = GIROSCOPE_x;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
+    LOCK_acquire(SYMPTOMS1_sem);
+    x = GIROSCOPE_x;
+    LOCK_release(SYMPTOMS1_sem);
     return x;
 }
 
 float GIROSCOPE_get_Y(void) {
-    configASSERT(SYMPTOMS1_sem != NULL);
     float y = -1;
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        y = GIROSCOPE_y;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
+    LOCK_acquire(SYMPTOMS1_sem);
+    y = GIROSCOPE_y;
+    LOCK_release(SYMPTOMS1_sem);
     return y;
 }
 
 float GIROSCOPE_get_Z(void) {
-    configASSERT(SYMPTOMS1_sem != NULL);
     float z = -1;
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        z = GIROSCOPE_z;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
+    LOCK_acquire(SYMPTOMS1_sem);
+    z = GIROSCOPE_z;
+    LOCK_release(SYMPTOMS1_sem);
     return z;
 }
 
 void WHEEL_set(int position) {
-    configASSERT(SYMPTOMS1_sem != NULL);
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        WHEEL_position = position;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
+    LOCK_acquire(SYMPTOMS1_sem);
+    WHEEL_position = position;
+    LOCK_release(SYMPTOMS1_sem);
 }
 
 int WHEEL_get(void) {
-    configASSERT(SYMPTOMS1_sem != NULL);
     int position = -1;
-    if (xSemaphoreTake(SYMPTOMS1_sem, portMAX_DELAY) == pdTRUE) {
-        position = WHEEL_position;
-        xSemaphoreGive(SYMPTOMS1_sem);
-    }
+    LOCK_acquire(SYMPTOMS1_sem);
+    position = WHEEL_position;
+    LOCK_release(SYMPTOMS1_sem);
     return position;
 }
 
 void WHEEL_grab(bool is_grabbed) {
-    configASSERT(SYMPTOMS2_sem != NULL);
-    if (xSemaphoreTake(SYMPTOMS2_sem, portMAX_DELAY) == pdTRUE) {
-        WHEEL_status_grab = is_grabbed;
-        xSemaphoreGive(SYMPTOMS2_sem);
-    }
+    LOCK_acquire(SYMPTOMS2_sem);
+    WHEEL_status_grab = is_grabbed;
+    LOCK_release(SYMPTOMS2_sem);
 }
 
 bool WHEEL_is_grabbed(void) {
-    configASSERT(SYMPTOMS2_sem != NULL);
     bool is_grabbed = false;
-    if (xSemaphoreTake(SYMPTOMS2_sem, portMAX_DELAY) == pdTRUE) {
-        is_grabbed = WHEEL_status_grab;
-        xSemaphoreGive(SYMPTOMS2_sem);
-    }
+    LOCK_acquire(SYMPTOMS2_sem);
+    is_grabbed = WHEEL_status_grab;
+    LOCK_release(SYMPTOMS2_sem);
     return is_grabbed;
 }
 

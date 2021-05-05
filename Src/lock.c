bba7165842144d0bab30e73f2c9ced853ea7c359
Javinator9889 <dev@javinator9889.com>
2021-05-05 10:26:14 +0200

    lock: added documentation

--- Src/lock.c
+++ Src/lock.c
@@ -24,31 +24,65 @@
 #include <semphr.h>
 #include "task.h"
 
-
-SemaphoreHandle_t LOCK_create(StaticSemaphore_t *mutexBuffer) {
+/**
+ * @brief creates a new lock (mutex) using FreeRTOS API. In addition,
+ *        some health checks are performed in order to return a valid
+ *        lock or, in other case, to block the execution forever
+ *        (both configASSERT will block if the condition is not met).
+ * 
+ *        In addition, the ability to create static mutexes is given
+ *        to the function if #mutexBuffer is not NULL and if
+ *        #configSUPPORT_STATIC_ALLOCATION equals 1. In other case,
+ *        a lock allocated in heap will be created.
+ * 
+ * @param mutexBuffer pointer to the static semaphore memory region.
+ *                    NULL if wanna create a heap-based semaphore.
+ * @return Lock_t - the created lock.
+ */
+Lock_t LOCK_create(StaticSemaphore_t *mutexBuffer) {
     SemaphoreHandle_t xSemaphore = NULL;
     BaseType_t xReturned;
 
-    #if defined(configSUPPORT_STATIC_ALLOCATION) && (configSUPPORT_STATIC_ALLOCATION == 1)
+#if defined(configSUPPORT_STATIC_ALLOCATION) && (configSUPPORT_STATIC_ALLOCATION == 1)
     if (mutexBuffer != NULL) xSemaphore = xSemaphoreCreateMutexStatic(mutexBuffer);
     else
-    #endif
+#endif
     xSemaphore = xSemaphoreCreateMutex();
 
     configASSERT(xSemaphore != NULL);    
     configASSERT(xSemaphoreGive(xSemaphore) != pdTRUE);
     
-    return xSemaphore;
+    return (Lock_t) xSemaphore;
 }
 
-inline void LOCK_destroy(SemaphoreHandle_t sem) {
-    vSemaphoreDelete(sem);
+/**
+ * @brief destroys the given lock (release from memory). After called,
+ *        the memory will be empty and the lock cannot be used anymore.
+ * 
+ * @param sem the lock to destroy.
+ */
+inline void LOCK_destroy(Lock_t sem) {
+    vSemaphoreDelete((SemaphoreHandle_t) sem);
 }
 
-inline long LOCK_acquire(SemaphoreHandle_t sem) {
-    return xSemaphoreTake(sem, portMAX_DELAY);
+/**
+ * @brief tries to acquire the given lock, blocking forever if necessary.
+ * 
+ * @param sem the lock to acquire.
+ * @return long - #pdTRUE if the semaphore was acquired. #pdFALSE otherwise.
+ */
+inline long LOCK_acquire(Lock_t sem) {
+    configASSERT(sem != NULL);
+    return xSemaphoreTake((SemaphoreHandle_t) sem, portMAX_DELAY);
 }
 
-inline void LOCK_release(SemaphoreHandle_t sem) {
-    xSemaphoreGive(sem);
+/**
+ * @brief tries to release the given lock, blocking forever if empty (lock is
+ *        NULL).
+ * 
+ * @param sem the lock to release.
+ */
+inline void LOCK_release(Lock_t sem) {
+    configASSERT(sem != NULL);
+    xSemaphoreGive((SemaphoreHandle_t) sem);
 }
